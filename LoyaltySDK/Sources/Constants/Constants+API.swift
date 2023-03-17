34a71ae2fc775be50812be83c44500492ea07854
David Bui <ducbm0512@gmail.com>
2023-03-17 14:41:02 +0700

    [Chore] - Create Constants and Constants+API
    - Create class NimbleLoyaltySDK

---
+++ LoyaltySDK/Sources/Constants/Constants+API.swift
@@ -0,0 +1,13 @@
+//
+//  Constants+API.swift
+//  LoyaltySDK
+//
+//  Created by David Bui on 17/03/2023.
+//
+
+import Foundation
+
+extension Constants.API {
+
+    static let baseURL: String = "http://cx-demo.nimblehq.co/api/"
+}
--- LoyaltySDK/Sources/SDK/NimbleLoyaltySDK.swift
+++ LoyaltySDK/Sources/SDK/NimbleLoyaltySDK.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+public final class NimbleLoyaltySDK {
+
+    public static let shared = NimbleLoyaltySDK()
+
+    private(set) var clientId: String = ""
+
+    private init() {}
+
+    public func setClientId(_ clientId: String) {
+        guard clientId.isEmpty else {
+            fatalError("Client Id must not be empty")
+        }
+        self.clientId = clientId
+    }
+}
