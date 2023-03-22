1b7ea9cefc1f0cd224149b7827c544089adf970a
David Bui <ducbm0512@gmail.com>
2023-03-22 09:27:43 +0700

    [Chore] Remove redundant code
    - Implement signIn function by using AuthenticationServices, retrieve access code, call API to get token and save in Keychain
    - Update example

--- Example/Example/ContentView.swift
+++ Example/Example/ContentView.swift
@@ -5,7 +5,10 @@
 //  Created by David Bui on 20/03/2023.
 //
 
+import NimbleLoyalty
 import SwiftUI
+import UIKit
+import WebKit
 
 struct ContentView: View {
     var body: some View {
@@ -13,14 +16,14 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello Nimble!")
         }
         .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                NimbleLoyalty.shared.setClientId("MRC6bwFASOm0sJYPYwVEt2KD51bkAtdgS7ltqeDYUf4")
+                NimbleLoyalty.shared.signIn()
+            }
+        }
     }
 }
